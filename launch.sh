1d69437884c02b006526a389faee5c9d096a1d6e
Canyuan Tian <ctian3@hawk.iit.edu>
2015-10-04 21:34:20 -0500

    fix the problem that page cannot load

--- launch.sh
+++ launch.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-echo "Please enter ami image id:"
-read id
-echo "Please enter cout:"
-read cnt
-echo "Please enter instance type:"
-read type
-echo "Please enter security group:"
-read sg
-echo "Please enter subnet id:"
-read sn
-echo "Please enter key name:"
-read key
-
-aws ec2 run-instances --image-id $id --count $cnt --instance-type $type --security-group-ids $sg --subnet-id $sn --key-name $key --associate-public-ip-address --user-data file:..//Environment-Setup/install-env.sh --debug
-
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://../Environment-Setup/install-env.sh --debug
