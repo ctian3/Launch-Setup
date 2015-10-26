c00d48ac1c6831a120c3743e277039886e97b015
Canyuan Tian <ctian3@hawk.iit.edu>
2015-10-26 00:13:09 -0500

    update to install-webserver.sh

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://../Environment-Setup/install-env.sh --debug
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address --user-data file://../Environment-Setup/install-webserver.sh --debug
